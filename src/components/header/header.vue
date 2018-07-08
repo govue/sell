25203ae5d749900548a0219ff35b6ebbdd3e094a
qubo <govuecn@163.com>
2018-07-09 06:37:54 +0800

    修改入口文件，能正常运行

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -1,11 +1,11 @@
 <template>
-
+  <div class="header">
+    我是header
+  </div>
 </template>
 
 <script type="text/ecmascript-6">
-    export default {
-        name: "header.vue"
-    }
+    export default {};
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue';
+import VueRouter from 'vue-router';
 import App from './App';
 
 /* eslint-disable no-new */
-new Vue({
-  el: 'body',
-  component: {App}
+Vue.use(VueRouter);
+
+let app = Vue.extend(App);
+
+let router = new VueRouter({
+  linkActiveClass: 'active'
 });
+
+router.start(app, '#app');
