6cfa79bdaa649715163c812474b3c102eae8576b
qubo <govuecn@163.com>
2018-07-11 21:32:58 +0800

    header布局。。。

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -33,13 +33,35 @@
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+
+    @import "../../common/stylus/mixin.styl"
+
     .header
         color: #fff
         background: #000
         .content-wrapper
             padding: 24px 12px 18px 24px
+            font-size: 0
             .avatar
                 display: inline-block
             .content
                 display: inline-block
+                font-size: 14px
+                margin-left: 16px
+                .title
+                    margin: 2px 0 8px 0
+                    .brand
+                        display: inline-block
+                        vertical-align: top
+                        width: 30px
+                        height: 18px
+                        bg-image(brand)
+                        background-size: 30px 18px
+                        background-repeat: no-repeat
+                    .name
+                        margin-left: 6px
+                        font-size: 16px
+                        line-height: 18px
+                        font-weight: bold
+
 </style>
