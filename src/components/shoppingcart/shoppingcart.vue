eb7009b238178609e2e8f7d15143ad5a0dd9a727
qubo <govuecn@163.com>
2018-08-15 23:12:23 +0800

    shoppingcart组件布局（left-wrapper）

--- src/components/shoppingcart/shoppingcart.vue
+++ src/components/shoppingcart/shoppingcart.vue
@@ -2,7 +2,7 @@
     <div class="shoppingcart">
         <div class="left-wrapper">
             <div class="logo">
-                <div class="background">
+                <div class="inner-box">
                     <i class="icon-shopping_cart"></i>
                 </div>
             </div>
@@ -23,10 +23,45 @@
 
 <style lang="stylus" rel="stylesheet/stylus">
     .shoppingcart
+        display: flex
         position: fixed
         left: 0
         bottom: 0
         background: #141d27
         width: 100%
         height: 46px
+        color: #fff
+        .left-wrapper
+            flex: 1
+            .logo
+                position: relative
+                top: -10px
+                display: inline-block
+                width: 58px
+                height: 58px
+                border-radius: 50%
+                background-color: #141d27
+                margin: 0 12px
+                box-sizing: border-box
+                padding: 6px
+                .inner-box
+                    width: 100%
+                    height: 100%
+                    border-radius: 50%
+                    background-color: #2B333B
+                    display: flex
+                    justify-content:center
+                    align-items: center
+            .total-price
+                display: inline-block
+            .deliveryPrice
+                display: inline-block
+        .right-wrapper
+            flex: 0 0 110px
+            width: 110px
+            height: 46px
+            line-height: 46px
+            text-align: center
+            background-color: #2B333B
+
 </style>
