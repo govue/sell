3e38eab82c1606e5042bc5971c5036870ee96d82
govue <govuecn@163.com>
2018-08-16 17:32:38 +0800

    shopping-cart布局调整

--- src/components/shoppingcart/shoppingcart.vue
+++ src/components/shoppingcart/shoppingcart.vue
@@ -30,7 +30,7 @@
         background: #141d27
         width: 100%
         height: 46px
-        color: #fff
+        color: rgba(255, 255, 255, 0.4)
         .left-wrapper
             flex: 1
             .logo
@@ -50,12 +50,21 @@
                     border-radius: 50%
                     background-color: #2B333B
                     display: flex
-                    justify-content:center
+                    justify-content: center
                     align-items: center
             .total-price
                 display: inline-block
+                height: 25px
+                line-height: 25px
+                vertical-align: top
+                margin-top: 10px
+                margin-right: 15px
+                padding-right: 15px
+                border-right: 1px solid rgba(255, 255, 255, 0.4)
             .deliveryPrice
                 display: inline-block
+                vertical-align: top
+                margin-top: 15px
         .right-wrapper
             flex: 0 0 110px
             width: 110px
